5a6c34ac3724ef0117347a1b920b98bf9cf84204
Burnham <georgiosburnham@gmail.com>
2022-12-19 09:47:09 +0100

    Test
    Test

---
+++ src/components/Experience.jsx
@@ -0,0 +1,39 @@
+import {
+  Float,
+  MeshReflectorMaterial,
+  PresentationControls,
+  Stage,
+} from "@react-three/drei";
+
+import { Suspense } from "react";
+import Chair_nella from "./Chair_nella";
+
+const Experience = () => {
+
+  return (
+    <PresentationControls speed={1.5} global polar={[-0.1, Math.PI / 4]}>
+      <Stage environment={"city"} intensity={0.6} contactShadow={false}>
+        <Suspense fallback={null}>
+          <Chair_nella />
+        </Suspense>
+      </Stage>
+      <mesh rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[170, 170]} />
+        <MeshReflectorMaterial
+          blur={[300, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={40}
+          roughness={1}
+          depthScale={1.2}
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color="#101010"
+          metalness={0.5}
+        />
+      </mesh>
+    </PresentationControls>
+  );
+};
+
+export default Experience;
